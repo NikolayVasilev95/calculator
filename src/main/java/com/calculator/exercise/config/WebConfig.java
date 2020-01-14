5723b44337afb768f017a619779792c9cf673995
Nikolay Vasilev <metalistite_two@abv.bg>
2020-01-14 14:20:24 +0200

    CrossOrigin

--- src/main/java/com/calculator/exercise/config/WebConfig.java
+++ src/main/java/com/calculator/exercise/config/WebConfig.java
@@ -8,6 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**").allowedOrigins("https://e-commerce-dev-front.000webhostapp.com/calculator").allowedMethods("GET");
+        registry.addMapping("/api/**").allowedOrigins("https://e-commerce-dev-front.000webhostapp.com/calculator").allowedMethods("*").allowedHeaders("*").allowCredentials(false).maxAge(3600);
     }
 }
